3318f3e139805693e335c40d1eae860de5cfbfed
sori883 <service@dlme.biz>
2021-11-08 23:34:44 +0900

    last

--- src/app/Http/Controllers/Bookmarks/BookmarkController.php
+++ src/app/Http/Controllers/Bookmarks/BookmarkController.php
@@ -25,6 +25,7 @@ use App\Bookmark\UseCase\ShowBookmarkListPageUseCase;
 use App\Http\Requests\CreateBookmarkRequest; 
 use App\Lib\LinkPreview\LinkPreview;
 use App\Bookmark\UseCase\CreateBookmarkUseCase;
+use App\Http\Requests\UpdateBookmarkRequest;
 
 class BookmarkController extends Controller
 {
@@ -166,18 +167,8 @@ class BookmarkController extends Controller
      * @return Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      * @throws ValidationException
      */
-    public function update(Request $request, int $id)
+    public function update(UpdateBookmarkRequest  $request, int $id)
     {
-        if (Auth::guest()) {
-            // @note ここの処理はユーザープロフィールでも使われている
-            return redirect('/login');
-        }
-
-        Validator::make($request->all(), [
-            'comment' => 'required|string|min:10|max:1000',
-            'category' => 'required|integer|exists:bookmark_categories,id',
-        ])->validate();
-
         $model = Bookmark::query()->findOrFail($id);
 
         if ($model->can_not_delete_or_edit) {
